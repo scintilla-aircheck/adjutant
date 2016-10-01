78ff18d72ec26ec866b90964a56c049db9a313f7
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-10-01 15:15:37 -0700

    Add basic library for controlling the UART mux circuit

---
+++ src/UARTMux.cpp
@@ -0,0 +1,46 @@
+#include "UARTMux.h"
+
+
+UARTMux::UARTMux(byte ext_select, byte int_select)
+{
+    ExtSelectPin_ = ext_select;
+    IntSelectPin_ = int_select;
+}
+
+
+UARTMux::~UARTMux() {}
+
+
+void UARTMux::Begin()
+{
+    // Set pin modes
+    pinMode(ExtSelectPin_, OUTPUT);
+    pinMode(IntSelectPin_, OUTPUT);
+
+    // Set default pin states
+    digitalWrite(ExtSelectPin_, LOW);
+    digitalWrite(IntSelectPin_, LOW);
+}
+
+
+void UARTMux::Select(ETarget target)
+{  
+    // Wait for output buffer to clear
+    Serial.flush();
+
+    if (target == ETarget::FTDI)
+    {
+        digitalWrite(ExtSelectPin_, LOW);
+        digitalWrite(IntSelectPin_, LOW);
+    }
+    else if (target == ETarget::SDS021)
+    {
+        digitalWrite(ExtSelectPin_, HIGH);
+        digitalWrite(IntSelectPin_, LOW);
+    }
+    else if (target == ETarget::SKM61)
+    {
+        digitalWrite(ExtSelectPin_, HIGH);
+        digitalWrite(IntSelectPin_, HIGH);
+    }
+} 
---
+++ src/UARTMux.h
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+
+class UARTMux
+{
+public:
+    UARTMux(byte ext_select, byte int_select);
+    ~UARTMux();
+
+    enum class ETarget
+    {
+        FTDI,
+        SDS021,
+        SKM61
+    };
+
+    // Initializes pin states
+    void Begin();
+
+    // Selects a UARTMux target connection
+    void Select(ETarget target);
+
+private:
+    byte ExtSelectPin_, IntSelectPin_;
+};
