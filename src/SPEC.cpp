62f71320dbddf9e4ca52b9547ab127347f356459
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-10-04 20:56:13 -0700

    Add target string to SPEC configurations

--- src/SPEC.cpp
+++ src/SPEC.cpp
@@ -6,12 +6,13 @@ namespace SPEC
 		LMP91000::ETIAGain tia_gain, LMP91000::ERLoad r_load, bool ext_ref,
 		LMP91000::EIntZero int_zero, bool bias_sign, LMP91000::EBias bias,
 		bool fet_short, LMP91000::EOpMode op_mode,
-		int range, double v_min, double v_max) :
+		String target, int range, double v_min, double v_max) :
 		LMP91000::Configuration(
 			tia_gain, r_load, ext_ref,
 			int_zero, bias_sign, bias,
 			fet_short, op_mode)
 	{
+		Target = target;
 		Range = range;
 		Vmin = v_min;
 		Vmax = v_max;
--- src/SPEC.h
+++ src/SPEC.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <Arduino.h>
 #include "LMP91000.h"
 
 namespace SPEC
 {
     struct Configuration : public LMP91000::Configuration
     {
+        // Target gas
+        String Target;
+
         // Sensor range (in ppm)
         int Range;
 
@@ -19,7 +23,7 @@ namespace SPEC
             LMP91000::ETIAGain tia_gain, LMP91000::ERLoad r_load, bool ext_ref,
             LMP91000::EIntZero int_zero, bool bias_sign, LMP91000::EBias bias,
             bool fet_short, LMP91000::EOpMode op_mode,
-            int range, double v_min, double v_max);
+            String target, int range, double v_min, double v_max);
     };
 
     static const Configuration CO = Configuration(
@@ -31,6 +35,7 @@ namespace SPEC
         LMP91000::EBias::d1pct,
         false,
         LMP91000::EOpMode::ThreeCell,
+        "CO",
         1000,
         0.5,
         2.16
@@ -45,6 +50,7 @@ namespace SPEC
         LMP91000::EBias::d1pct,
         false,
         LMP91000::EOpMode::ThreeCell,
+        "H2S",
         50,
         0.5,
         1.70
@@ -59,6 +65,7 @@ namespace SPEC
         LMP91000::EBias::d8pct,
         false,
         LMP91000::EOpMode::ThreeCell,
+        "NO2",
         20,
         1.25,
         1.04
@@ -73,6 +80,7 @@ namespace SPEC
         LMP91000::EBias::d8pct,
         false,
         LMP91000::EOpMode::ThreeCell,
+        "S02",
         20,
         1.25,
         1.46
@@ -87,6 +95,7 @@ namespace SPEC
         LMP91000::EBias::d1pct,
         false,
         LMP91000::EOpMode::ThreeCell,
+        "O3",
         20,
         1.25,
         1.11
@@ -101,6 +110,7 @@ namespace SPEC
         LMP91000::EBias::d4pct,
         false,
         LMP91000::EOpMode::ThreeCell,
+        "ETOH",
         1000,
         0.5,
         2.18
